00c4cf511345e9b9ed5a3deb787cab290da2c89f
ZubairQazi <zqazi004@ucr.edu>
2020-04-17 22:03:44 -0700

    Finished Lab 4 Part 2

---
+++ Lab4_SMs/turnin/zqazi004_lab4_part2.c
@@ -0,0 +1,105 @@
+/*	Author: Zubair
+ *  Partner(s) Name: 
+ *	Lab Section: 024
+ *	Assignment: Lab #4  Exercise #2
+ *	Exercise Description: [optional - include for your own benefit]
+ *
+ *	I acknowledge all content contained herein, excluding template or example
+ *	code, is my own original work.
+ */
+#include <avr/io.h>
+#ifdef _SIMULATE_
+#include "simAVRHeader.h"
+#endif
+
+//#include "RIMS.h"
+enum States {Start, Init, Add_Press, Add_Release, Sub_Press, Sub_Release, Reset} state;
+
+void Tick() {
+    
+    unsigned char tempA = PINA;
+    unsigned char tempC = PORTC;
+
+    switch(state) {
+        case Start:
+            state = Init;
+            break;
+
+        case Init:
+            if (tempA == 1)
+                state = Add_Press;
+            else if (tempA == 2) 
+                state = Sub_Press;
+            else if (tempA == 3)
+                state = Reset;
+            break;
+
+        case Add_Press:
+            state = Add_Release;
+            break;
+
+        case Add_Release:
+            if (tempA == 0)
+                state = Init;
+            else if (tempA == 1)
+                state = Add_Release;
+            else if (tempA == 3)
+                state = Reset;
+            break;
+
+        case Sub_Press:
+            state = Sub_Release;
+            break;
+
+        case Sub_Release:
+            if (tempA == 0)
+                state = Init;
+            else if (tempA == 2)
+                state = Sub_Release;
+            else if (tempA == 3)
+                state = Reset;
+            break;
+
+        case Reset:
+            state = Reset;
+            break;
+
+        default:
+            state = Init;
+            break;
+    }
+
+    switch(state) {
+        case Add_Press:
+            if (tempC < 9)
+                tempC = tempC + 1;
+            break;
+
+        case Sub_Press:
+            if (tempC > 0)
+                tempC = tempC - 1;
+            break;
+
+        case Reset:
+            tempC = 0x00;
+            break;
+
+        default:
+            break;
+    }
+
+    PORTC = tempC;
+
+}
+
+int main(void) {
+    /* Insert DDR and PORT initializations */
+    DDRA = 0x00; PORTA = 0xFF;
+    DDRC = 0xFF; PORTC = 0x07;
+
+    /* Insert your solution below */
+    while (1) {
+        Tick();
+    }
+    return 1;
+}
