15867fd2eb3f2df1c1f12921f1831955a3eaf441
ZubairQazi <zqazi004@ucr.edu>
2020-04-23 10:57:21 -0700

    Finished Lab 5

---
+++ Lab5_Chip/turnin/zqazi004_lab5_part1.c
@@ -0,0 +1,50 @@
+// https://drive.google.com/drive/folders/1s9MdbH1g7w5wyXiyYg8XfnSyaMMarmXs?usp=sharing
+
+
+/*	Author: zubair
+ *  Partner(s) Name: 
+ *	Lab Section:
+ *	Assignment: Lab #5  Exercise #1
+ *	Exercise Description: [optional - include for your own benefit]
+ *
+ *	I acknowledge all content contained herein, excluding template or example
+ *	code, is my own original work.
+ */
+#include <avr/io.h>
+#ifdef _SIMULATE_
+#include "simAVRHeader.h"
+#endif
+
+int main(void) {
+    /* Insert DDR and PORT initializations */
+    DDRA = 0x00; PORTA = 0xFF;
+    DDRC = 0xFF; PORTC = 0x00;
+
+    unsigned char tempA = 0x00;
+    unsigned char tempC = 0x00;
+
+    /* Insert your solution below */
+    while (1) {
+        tempA = ~PINA & 0x0F;
+        tempC = 0x00;
+
+        if (tempA <= 2 && tempA >= 1) 
+            tempC = tempC | 0x20;
+        else if (tempA <= 4 && tempA >= 3)
+            tempC = tempC | 0x30;
+        else if (tempA <= 6 && tempA >= 5)
+            tempC = tempC | 0x38;
+        else if (tempA <= 9 && tempA >= 7)
+            tempC = tempC | 0x3C;
+        else if (tempA <= 12 && tempA >= 10)
+            tempC = tempC | 0x3E;
+        else if (tempA <= 15 && tempA >= 13)
+            tempC = tempC | 0x3F;
+
+        if (tempA <= 4)
+            tempC = tempC | 0x40;
+
+        PORTC = tempC;
+    }
+    return 1;
+}
---
+++ Lab5_Chip/turnin/zqazi004_lab5_part3.c
@@ -0,0 +1,107 @@
+// https://drive.google.com/drive/folders/1s9MdbH1g7w5wyXiyYg8XfnSyaMMarmXs?usp=sharing
+
+
+/*	Author: Zubair
+ *  Partner(s) Name: 
+ *	Lab Section: 024
+ *	Assignment: Lab #5  Exercise #3
+ *	Exercise Description: [optional - include for your own benefit]
+ *
+ *	I acknowledge all content contained herein, excluding template or example
+ *	code, is my own original work.
+ */
+#include <avr/io.h>
+#ifdef _SIMULATE_
+#include "simAVRHeader.h"
+#endif
+
+//#include "RIMS.h"
+enum States {Start, Init, Middle, Left, Right, Reset} state;
+
+void Tick() {
+    
+    unsigned char tempA = ~PINA;
+    unsigned char tempC = PORTC;
+
+    switch(state) {
+        case Start:
+            state = Init;
+            break;
+
+        case Init:
+            if (tempA == 0x01)
+                state = Middle;
+            else
+                state = Init;
+            break;
+
+        case Middle:
+            if (tempA == 0x01)
+                state = Left;
+            else
+                state = Middle;
+            break;
+
+        case Left:
+            if (tempA == 0x01)
+                state = Right;
+            else
+                state = Left;
+            break;
+
+        case Right:
+            if (tempA == 0x01)
+                state = Init;
+            else
+                state = Left;
+            break;
+
+        case Reset:
+            state = Init;
+            break;
+
+        default:
+            state = Init;
+            break;
+    }
+
+    switch(state) {
+        case Init:
+            tempC = 0x3F;
+            break;
+
+        case Middle:
+            tempC = 0x0C;
+            break;
+
+        case Left:
+            tempC = 0x03;
+            break;
+
+        case Right:
+            tempC = 0x30;
+            break;
+        
+        case Reset:
+            tempC = 0x00;
+            break;
+
+        default:
+            break;
+    }
+
+    PORTC = tempC;
+
+}
+
+int main(void) {
+    /* Insert DDR and PORT initializations */
+    DDRA = 0x00; PORTA = 0xFF;
+    DDRC = 0xFF; PORTC = 0x00;
+
+    /* Insert your solution below */
+    while (1) {
+        Tick();
+    }
+    return 1;
+}
